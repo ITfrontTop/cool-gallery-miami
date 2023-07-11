c37b88d1f9b9d5898e155933e84329e821c3a8df
ITfrontTop <Fine1337@ukr.net>
2023-07-12 02:41:00 +0300

    finish slider

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,8 @@ function sprite() {
 
 function scripts() {
     return src([
-        'app/js/main.js',
+        'node_modules/swiper/swiper-bundle.js',
+        'app/js/main.js'
     ])
         .pipe(concat('main.min.js'))
         .pipe(uglify())
